89486d5de855890196362e20b83f469162b97d60
boconnell2 <bmoconnell15@gmail.com>
2020-10-27 16:53:46 -0400

    commit tree 251c93ba20798763d53284303e0f9291ebf3a189

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
 # code here!
+require 'pry'
+
+class School
+    attr_accessor :name, :roster
+
+    def initialize(name, roster={})
+        @name = name
+        @roster = roster
+    end
+
+    def add_student(name, grade)
+        if @roster.key?(grade)
+            @roster[grade].push(name)
+        else
+            @roster[grade] = []
+            @roster[grade].push(name)
+        end
+    end
+
+    def grade(grade_desired)
+        @roster[grade_desired]
+    end
+
+    def sort
+        @roster.each do |k,v|
+            @roster[k] = v.sort
+        end
+    end
+
+end
+
+
